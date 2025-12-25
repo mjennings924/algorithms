2d8b2fd9af6729f5a9d48bd604cdab8af35ba888
mjennings924 <mitchellj@outlook.jp>
2025-12-25 13:46:18 +1100

    add insertion_sort

---
+++ insertion_sort.py
@@ -0,0 +1,13 @@
+A = [5, 2, 45, -2, 5, 86, 1, 0, -4, 10]
+
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        for j in range(i, 0, -1):
+            if arr[j-1] > arr[j]:
+                arr[j-1], arr[j] = arr[j], arr[j-1]
+            else:
+                break
+
+insertion_sort(A)
+print(A)
